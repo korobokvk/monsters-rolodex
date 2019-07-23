74a82e02a5e58a86f621e6ed73b04d47a10341e7
Maksym Zakshevskyi <mzakshevskyi@lohika.com>
2019-07-24 00:10:30 +0300

    Updates

---
+++ precache-manifest.cdbe132fa0fd14141c0acac5314935ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b36590dfe8e69df65153e72bca959e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f99c18017891de5ac063",
+    "url": "/monsters-rolodex/static/css/main.64d21ec4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f99c18017891de5ac063",
+    "url": "/monsters-rolodex/static/js/main.dc1c63bc.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
